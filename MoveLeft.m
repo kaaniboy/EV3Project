46f48b7f01b7c3a747571ff69585eed70a62c060
Kaan Aksoy (Student) <kaksoy@sundevils.asu.edu>
2016-10-12 10:29:34 -0700

    Fixed issue with turning functions

--- MoveLeft.m
+++ MoveLeft.m
@@ -1,7 +1,9 @@
 function MoveLeft(brick)
     brick.MoveMotor('B', 100);
+    brick.MoveMotor('A', -100);
     pause(1);
     brick.StopMotor('B');
+    brick.StopMotor('A');
 end
 
 
--- Vehicle.m
+++ Vehicle.m
@@ -1,8 +1,9 @@
 %Left Motor: A
 %Right Motor: B
-NAME = 'lion';
-
-brick = ConnectBrick(NAME);
+NAME = 'Lion';
+brick = ConnectBrick('Lion');
 
 MoveLeft(brick);
 MoveRight(brick);
+
+DisconnectBrick(brick);
