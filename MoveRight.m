88c2cd0083e1460fc0744fd38fcb0cca559a9a43
Kaan Aksoy (Student) <kaksoy@sundevils.asu.edu>
2016-10-19 10:22:58 -0700

    Updated MoveLeft function that incrementally turns

--- MoveRight.m
+++ MoveRight.m
@@ -1,7 +1,13 @@
 function MoveRight(brick)
-    brick.MoveMotorAngleRel('A', 50, 720);
-    brick.MoveMotorAngleRel('B', 50, -720);
-    %pause(1);
-    %brick.StopMotor('A');
-    %brick.StopMotor('B');
+    currentAngle = brick.GyroAngle(4);
+    desiredAngle = currentAngle + 90;
+    while(currentAngle <= desiredAngle)
+        brick.MoveMotorAngleRel('A', 50, 5);
+        brick.MoveMotorAngleRel('B', 50, -5);
+        pause(.1);
+        currentAngle = brick.GyroAngle(4);
+        display(currentAngle);
+    end
+    pause(1);
+    brick.StopAllMotors();
 end
--- Vehicle.m
+++ Vehicle.m
@@ -1,19 +1,28 @@
 %Left Motor: A
 %Right Motor: B
 NAME = 'Lion';
-brick = ConnectBrick('Lion');
+%brick = ConnectBrick('Lion');
 
 while 1
     dist = brick.UltrasonicDist(1);
-    if(dist >= 10)
+    pause(0.1);
+    display(dist);
+    angle = brick.GyroAngle(4);
+    display(angle);
+    
+    if(dist >= 20)
         brick.MoveMotor('A', 50);
         brick.MoveMotor('B', 50);
-        pause(0.4);
-        brick.StopMotor('A', 50);
-        brick.StopMotor('B', 50);
     else
+        brick.StopAllMotors();
         MoveRight(brick);
     end
+    
+    if(brick.TouchPressed(2) == 1)
+        %DisconnectBrick(brick);
+        brick.StopAllMotors();
+        break;
+    end
+    
+    
 end
-
-DisconnectBrick(brick);
