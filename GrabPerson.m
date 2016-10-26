32e394e3138626db82608088725c6f53192a8d07
Kaan Aksoy (Student) <kaksoy@sundevils.asu.edu>
2016-10-26 10:35:05 -0700

    Wrote basic color detection function

--- GrabPerson.m
+++ GrabPerson.m
@@ -1,3 +1,7 @@
 function GrabPerson(brick)
-    brick.MoveMotorAngleRel('C', 81, 5);
+    brick.MoveMotorAngleRel('C', 200, 5);
+    pause(0.1);
+    brick.MoveMotorAngleRel('C', 200, 5);
+    pause(0.1);
+    brick.MoveMotorAngleRel('C', 200, 5);
 end
