67a923a659291fee57bba49cb791a33784bb3832
Kaan Aksoy (Student) <kaksoy@sundevils.asu.edu>
2016-10-26 09:28:47 -0700

    It Works. Time for better

--- MoveRight.m
+++ MoveRight.m
@@ -1,15 +1,15 @@
 function MoveRight(brick)
     currentAngle = brick.GyroAngle(4);
-    desiredAngle = currentAngle + 90;
+    desiredAngle = currentAngle + 82;
     while(currentAngle <= desiredAngle)
         %brick.MoveMotorAngleRel('A', 50, 5);
         %brick.MoveMotorAngleRel('B', 50, -5);
-        brick.MoveMotor('A', 10);
-        brick.MoveMotor('B', -10);
-        pause(.05);
+        brick.MoveMotor('A', 15);
+        brick.MoveMotor('B', -15);
+        pause(.01);
         currentAngle = brick.GyroAngle(4);
         display(currentAngle);
     end
     brick.StopAllMotors();
-    pause(1);
+    pause(0.5);
 end
--- Vehicle.m
+++ Vehicle.m
@@ -1,28 +1,30 @@
 %Left Motor: A
 %Right Motor: B
-NAME = 'Lion';
-%brick = ConnectBrick('Lion');
+NAME = 'Mirage';
+%brick = ConnectBrick('Mirage');
+
+brick.StopAllMotors();
 
 while 1
-    dist = brick.UltrasonicDist(1);
+        pause(0.1);        
+        dist = brick.UltrasonicDist(1);
+        display(dist);
+        angle = brick.GyroAngle(4);
+        display(angle);
 
-    display(dist);
-    angle = brick.GyroAngle(4);
-    display(angle);
-    
-    if(dist >= 20)
-        brick.MoveMotor('A', 52);
-        brick.MoveMotor('B', 50);
-    else
-        brick.StopAllMotors();
-        MoveRight(brick);
-    end
-    
-    if(brick.TouchPressed(2) == 1)
-        %DisconnectBrick(brick);
-        brick.StopAllMotors();
-        break;
-    end
-    
-    pause(0.2);
+        if(dist >= 20)
+            brick.MoveMotor('A', 52);
+            brick.MoveMotor('B', 50);
+        else
+            brick.StopAllMotors();
+            MoveRight(brick);
+            display('AAA');
+        end
+
+        if(brick.TouchPressed(2) == 1)
+            brick.StopAllMotors();
+            break;
+        end
+
+        pause(0.2);
 end
